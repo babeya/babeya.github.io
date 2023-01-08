b36ee69d3327f6fa823b8e5fa4335d7087675f74
ababey <git@ababey.com>
2023-01-08 23:27:35 +0100

    Animate timeline

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import { TransitionGroup } from "react-transition-group";
+
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
+import Collapse from "@mui/material/Collapse";
 
 import useTimelineData from "./useTimelineData";
 import { JobCard, ProjectCard } from "./EventCards";
@@ -25,17 +28,17 @@ const Timeline = () => {
       <Divider sx={{ marginY: 2 }} />
 
       <Box sx={{ borderLeft: "2px solid grey" }}>
-        {timelineData.map(({ node }) =>
-          node?.typename === "job" ? ( // TODO: auto generate __typename
-            <JobCard job={node} key={node.id} selectedTags={filters.tags} />
-          ) : (
-            <ProjectCard
-              project={node}
-              key={node.id}
-              selectedTags={filters.tags}
-            />
-          )
-        )}
+        <TransitionGroup>
+          {timelineData.map(({ node }) => (
+            <Collapse key={node.id} mountOnEnter unmountOnExit>
+              {node?.typename === "job" ? ( // TODO: auto generate __typename
+                <JobCard job={node} selectedTags={filters.tags} />
+              ) : (
+                <ProjectCard project={node} selectedTags={filters.tags} />
+              )}
+            </Collapse>
+          ))}
+        </TransitionGroup>
       </Box>
     </Paper>
   );
