958183ee58307c48c8e80361487f92d58d498ab5
ababey <git@ababey.com>
2023-01-16 13:24:04 +0100

    Improve mobile display #1

--- src/components/Timeline/EventCards/BaseEventCard.tsx
+++ src/components/Timeline/EventCards/BaseEventCard.tsx
@@ -4,6 +4,7 @@ import Stack from "@mui/material/Stack";
 import Chip from "@mui/material/Chip";
 import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Fab from "@mui/material/Fab";
 
 type Props = {
@@ -22,17 +23,9 @@ const BaseEventCard = ({
   selectedTags,
 }: Props) => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        direction: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingY: 2,
-      }}
-    >
-      <Box sx={{ position: "relative" }}>
-        <Box sx={{ transform: "translate(-50%)" }}>
+    <Grid container direction="row" alignItems="center" justifyContent="center">
+      <Grid sx={{ position: "relative" }} xs={1}>
+        <Box sx={{ margin: 1 }}>
           <Fab
             size="medium"
             sx={{
@@ -44,28 +37,30 @@ const BaseEventCard = ({
             {icon}
           </Fab>
         </Box>
-      </Box>
-      <Card
-        sx={{
-          flex: 1,
-          padding: 2,
-        }}
-        elevation={3}
-      >
-        {children}
-        <Stack direction="row-reverse" spacing={1} marginY={1}>
-          {(tags || []).map((tag) => (
-            <Chip
-              label={tag}
-              key={tag}
-              size="small"
-              variant="outlined"
-              color={selectedTags.includes(tag) ? "primary" : undefined}
-            />
-          ))}
-        </Stack>
-      </Card>
-    </Box>
+      </Grid>
+      <Grid xs={12} md={11} sx={{ margin: 1 }}>
+        <Card
+          sx={{
+            flex: 1,
+            padding: 2,
+          }}
+          elevation={3}
+        >
+          {children}
+          <Stack direction="row-reverse" spacing={1} marginY={1}>
+            {(tags || []).map((tag) => (
+              <Chip
+                label={tag}
+                key={tag}
+                size="small"
+                variant="outlined"
+                color={selectedTags.includes(tag) ? "primary" : undefined}
+              />
+            ))}
+          </Stack>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
